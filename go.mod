9b97f78732cc579703fbaaac17282dc3eadef0fd
Turtlebole <babic.bojan62@gmail.com>
2023-11-08 02:17:38 +0100

    mongo connection string added

--- go.mod
+++ go.mod
@@ -7,27 +7,25 @@ require (
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
-	gorm.io/driver/postgres v1.3.10
-	gorm.io/gorm v1.23.10
+	go.mongodb.org/mongo-driver v1.13.0
 )
 
 require (
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.13.0 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
-	github.com/jackc/pgtype v1.12.0 // indirect
-	github.com/jackc/pgx/v4 v4.17.2 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- main.go
+++ main.go
@@ -1,92 +1,72 @@
 package main
 
 import (
-	"12factorapp/data"
-	"12factorapp/handlers"
 	"context"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
-	gorillaHandlers "github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	//Initialize the logger we are going to use, with prefix and datetime for every log
-	//As per 12 factor app the general place for app to log is the standard output.
-	//If you want to save the logs to a file run the app with the following command.
-	//
-	//	go run . >> output.txt
-	//
 	logger := log.New(os.Stdout, "[product-api] ", log.LstdFlags)
 
-	//Load .env file, generally used just to not fill up the environment with variables that are for a specific project
-	//Important notice is that .env file is not stored in version control (Git, Svn...) and every developer should create his .env file
-	//And fill it with variables for his system. After loading .env variables we can access them with os.Getenv()
+	// Load .env file to get configuration values
 	e := godotenv.Load()
 	if e != nil {
 		logger.Fatal(e)
 	}
-	//Reading from environment, if not set we will default it to 8080.
-	//This allows flexibility in different environments (for eg. when running multiple docker api's and want to override the default port)
-	port := os.Getenv("app_port")
+
+	// Read environment variables
+	port := os.Getenv("PORT")
+	mongoURL := os.Getenv("MONGODB_URL")
+
 	if len(port) == 0 {
 		port = "8080"
 	}
 
-	//Initialize the repository that uses the actual database. If the in memory counter part is to be used,
-	//swap out the call to 'NewPostgreSql' to 'NewInMemory' and rerun the program.
-	userRepo, err := data.NewInMemory(logger)
+	if len(mongoURL) == 0 {
+		logger.Fatal("MONGODB_URL is not defined in env file.")
+	}
 
+	// Initialize MongoDB client
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		logger.Fatal(err)
 	}
-	//Initialize the handler and inject said logger
-	productsHandler := handlers.New(logger, &userRepo)
+	defer client.Disconnect(context.Background())
 
-	//Initialize the router and add a middleware for all the requests
+	// Create a new router
 	router := mux.NewRouter()
-	router.Use(productsHandler.MiddlewareContentTypeSet)
 
-	getRouter := router.Methods(http.MethodGet).Subrouter()
-	getRouter.HandleFunc("/", productsHandler.GetUsers)
+	// router.HandleFunc("/users")
 
-	getAllRouter := router.Methods(http.MethodGet).Subrouter()
-	getAllRouter.HandleFunc("/all", productsHandler.GetAllUsers)
+	// CORS settings
+	cors := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}),
+	)
 
-	postRouter := router.Methods(http.MethodPost).Subrouter()
-	postRouter.HandleFunc("/", productsHandler.PostUsers)
-	postRouter.Use(productsHandler.MiddlewareUserValidation)
-
-	putRouter := router.Methods(http.MethodPut).Subrouter()
-	putRouter.HandleFunc("/{id:[0-9]+}", productsHandler.PutUsers)
-	putRouter.Use(productsHandler.MiddlewareUserValidation)
-
-	deleteHandler := router.Methods(http.MethodDelete).Subrouter()
-	deleteHandler.HandleFunc("/{id:[0-9]+}", productsHandler.DeleteUsers)
-
-	//Set cors. Generally you wouldn't like to set cors to a "*". It is a wildcard and it will match any source.
-	//Normally you would set this to a set of ip's you want this api to serve. If you have an associated frontend app
-	//you would put the ip of the server where the frontend is running. The only time you don't need cors is when you
-	//calling the api from the same ip, or when you are using the proxy (for eg. Nginx)
-	cors := gorillaHandlers.CORS(gorillaHandlers.AllowedOrigins([]string{"*"}))
-
-	//Initialize the server
-	server := http.Server{
-		Addr:         ":" + port,        // Addr optionally specifies the TCP address for the server to listen on, in the form "host:port". If empty, ":http" (port 80) is used.
-		Handler:      cors(router),      // handler to invoke, http.DefaultServeMux if nil
-		IdleTimeout:  120 * time.Second, // IdleTimeout is the maximum amount of time to wait for the next request when keep-alives are enabled.
-		ReadTimeout:  1 * time.Second,   // ReadTimeout is the maximum duration for reading the entire request, including the body. A zero or negative value means there will be no timeout.
-		WriteTimeout: 1 * time.Second,   // WriteTimeout is the maximum duration before timing out writes of the response.
+	// Initialize the HTTP server
+	server := &http.Server{
+		Addr:         ":" + port,
+		Handler:      cors(router),
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
 	}
 
 	logger.Println("Server listening on port", port)
-	//Distribute all the connections to goroutines
+
+	// Start the server in a goroutine
 	go func() {
 		err := server.ListenAndServe()
 		if err != nil {
@@ -94,19 +74,21 @@ func main() {
 		}
 	}()
 
+	// Set up signal handling for graceful shutdown
 	sigCh := make(chan os.Signal)
-	signal.Notify(sigCh, syscall.SIGINT)
-	signal.Notify(sigCh, syscall.SIGKILL)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
 
-	//When we receive an interrupt or kill, if we don't have any current connections the code will terminate.
-	//But if we do the code will stop receiving any new connections and wait for maximum of 30 seconds to finish all current requests.
-	//After that the code will terminate.
 	sig := <-sigCh
-	logger.Println("Received terminate, graceful shutdown", sig)
-	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	logger.Println("Received termination signal:", sig)
 
-	//Try to shutdown gracefully
-	if server.Shutdown(timeoutContext) != nil {
-		logger.Fatal("Cannot gracefully shutdown...")
+	// Create a context with a timeout for graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Shutdown the server gracefully
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Fatal("Graceful shutdown error:", err)
 	}
+
+	logger.Println("Server shut down gracefully")
 }
