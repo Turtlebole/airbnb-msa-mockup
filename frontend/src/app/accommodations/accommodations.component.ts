5ec7f7e1821f1fd89af95f5c9547f9149771f0c9
Turtlebole <babic.bojan62@gmail.com>
2023-11-13 17:50:16 +0100

    changed structure

---
+++ frontend/src/app/accommodations/accommodations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodations',
+  templateUrl: './accommodations.component.html',
+  styleUrls: ['./accommodations.component.css']
+})
+export class AccommodationsComponent {
+
+}
