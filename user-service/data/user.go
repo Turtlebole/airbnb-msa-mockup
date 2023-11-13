81b12566f6b66395db16b7f92909e1722455e747
Turtlebole <babic.bojan62@gmail.com>
2023-11-13 16:48:44 +0100

    Revert "Revert "project structure change""
    This reverts commit 964810b6da683f4dbb66dbaf8cc07677b6fa39fa.

---
+++ user-service/data/user.go
@@ -0,0 +1,68 @@
+package data
+
+import (
+	"encoding/json"
+	"github.com/go-playground/validator/v10"
+	"io"
+	"regexp"
+	"time"
+)
+
+// Defining the main struct for our API
+type User struct {
+	ID         int       `json:"id"`                       //specifies that in the incoming Body the field to map to this will be called "id"
+	First_Name *string   `json:"name" validate:"required"` //there are some integrated validation, for eg. this specifies that a value for name must be provided, otherwise it will not be valid
+	Last_Name  *string   `json:"description"`
+	Email      *string   `json:"price" validate:"gt=0"`
+	Address    *string   `json:"address"`
+	SKU        string    `json:"sku" validate:"required,sku"` //the tag "sku" is there so we can add custom validation
+	Created_On time.Time `json:"created_On"`
+	Updated_On time.Time `json:"updated_On"`
+	Deleted_On time.Time `json:"deleted_On"`
+}
+
+type Users []*User
+
+// Function to validate the incoming object from front.
+// NOTE: if the tag "sku" is not present in the struct anotations we will get an error
+func (u *User) Validate() error {
+	validate := validator.New()
+
+	err := validate.RegisterValidation("sku", validateSKU)
+	if err != nil {
+		return err
+	}
+
+	return validate.Struct(u)
+}
+
+// We use a regex to validate a custom look of sku-s
+// For eg: abc-abc-abc
+func validateSKU(fl validator.FieldLevel) bool {
+	re := regexp.MustCompile("[a-z]+-[a-z]+-[a-z]")
+	matches := re.FindAllString(fl.Field().String(), -1)
+
+	if len(matches) != 1 {
+		return false
+	}
+
+	return true
+}
+
+// Functions to encode and decode products to json and from json.
+// If we inject an interface we achieve dependancy injection, meaning that anything that implements this interface can be passed down
+// For us it will be ResponseWriter, but it also may be a file writer or something similar.
+func (u *Users) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(u)
+}
+
+func (u *User) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(u)
+}
+
+func (p *User) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
+}
