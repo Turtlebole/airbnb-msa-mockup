e6d1697f49946401c929131433b8226dd983db4f
Turtlebole <babic.bojan62@gmail.com>
2023-11-15 02:59:30 +0100

    jws commit revert

--- backend/user-service/main.go
+++ backend/user-service/main.go
@@ -1,53 +1,25 @@
 package main
 
 import (
-	"12factorapp/routes"
-	"context"
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/handlers"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
+
+	routes "backend/routes"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	logger := log.New(os.Stdout, "[product-api] ", log.LstdFlags)
-
-	// Load .env file to get configuration values
-	e := godotenv.Load()
-	if e != nil {
-		logger.Fatal(e)
-	}
-
-	// Read environment variables
 	port := os.Getenv("PORT")
-	mongoURL := os.Getenv("MONGODB_URL")
 
-	if len(port) == 0 {
-		port = "8080"
+	if port == "" {
+		port = "8000"
 	}
-
-	if len(mongoURL) == 0 {
-		logger.Fatal("MONGODB_URL is not defined in env file.")
-	}
-
-	// Initialize MongoDB client
-	clientOptions := options.Client().ApplyURI(mongoURL)
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		logger.Fatal(err)
-	}
-	defer client.Disconnect(context.Background())
 
 	router := gin.New()
 	router.Use(gin.Logger())
 
+	routes.AuthRoutes(router)
 	routes.UserRoutes(router)
 
 	// API-2
@@ -63,47 +35,4 @@ func main() {
 	})
 
 	router.Run(":" + port)
-
-	// CORS settings
-	cors := handlers.CORS(
-		handlers.AllowedOrigins([]string{"*"}),
-		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}),
-	)
-
-	// Initialize the HTTP server
-	server := &http.Server{
-		Addr:         ":" + port,
-		Handler:      cors(router),
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
-	}
-
-	logger.Println("Server listening on port", port)
-
-	// Start the server in a goroutine
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			logger.Fatal(err)
-		}
-	}()
-
-	// Set up signal handling for graceful shutdown
-	sigCh := make(chan os.Signal)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-
-	sig := <-sigCh
-	logger.Println("Received termination signal:", sig)
-
-	// Create a context with a timeout for graceful shutdown
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-
-	// Shutdown the server gracefully
-	if err := server.Shutdown(ctx); err != nil {
-		logger.Fatal("Graceful shutdown error:", err)
-	}
-
-	logger.Println("Server shut down gracefully")
 }
