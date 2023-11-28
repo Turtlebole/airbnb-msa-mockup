03b7dbe17072a7188f9a5a459b458dc3c38fbe41
Nenad Vukelic <nesa20035@gmail.com>
2023-11-28 02:49:16 +0100

    auth router

--- backend/user-service/routes/authRouter.go
+++ backend/user-service/routes/authRouter.go
@@ -8,11 +8,9 @@ import (
 )
 
 func AuthRoutes(routes *gin.Engine) {
-	routes.Use(func(c *gin.Context) {
-		middleware.Rbac(c)
-	})
 	routes.POST("/users/register", controller.Register())
 	routes.POST("/users/login", func(c *gin.Context) {
+		middleware.Rbac(c,"/users/login")
 		controller.Login(c)
 	})
 	routes.POST("/users/logout", controller.Logout())
