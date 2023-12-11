c8bdbfb7a79ef383e81045d431532c36ac2a1575
Nenad Vukelic <nesa20035@gmail.com>
2023-12-11 19:54:21 +0100

    hotfix
    -deleted password validation feature i was testing out

--- backend/user-service/models/user.go
+++ backend/user-service/models/user.go
@@ -1,24 +1,14 @@
 package models
 
 import (
-	"regexp"
-	"strings"
 	"time"
 
-	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type PasswordResetToken struct {
 	Token     string    `json:"token"`
 	ExpiresAt time.Time `json:"expires_at"`
-}
-
-var validate *validator.Validate
-
-func init() {
-	validate = validator.New()
-	validate.RegisterValidation("passwordStrength", passwordStrengthValidation)
 }
 
 type User struct {
@@ -36,24 +26,4 @@ type User struct {
 	Updated_at         time.Time           `json:"updated_at"`
 	User_id            string              `json:"user_id"`
 	PasswordResetToken *PasswordResetToken `json:"password_reset_token,omitempty"`
-}
-
-func passwordStrengthValidation(fl validator.FieldLevel) bool {
-	password, ok := fl.Field().Interface().(string)
-	if !ok {
-		return false
-	}
-
-	// Check for at least one capital letter
-	if !strings.ContainsAny(password, "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
-		return false
-	}
-
-	// Check for at least one special character
-	specialCharRegex := regexp.MustCompile(`[!@#$%^&*()-_=+{};:'",.<>?/\\|[\]~]`)
-	if !specialCharRegex.MatchString(password) {
-		return false
-	}
-
-	return true
 }
