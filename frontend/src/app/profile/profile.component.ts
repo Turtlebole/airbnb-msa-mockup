51a1f22f9773a4402e3d0afe0147c0b1e8e14da8
Bojan <babic.bojan62@gmail.com>
2023-12-26 01:13:28 +0100

    display reservations on profile

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -7,41 +7,81 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  profiles: any[] = []; // Define the type of your profile data here
-  token=localStorage.getItem('token')
-  userID: string|undefined;
+  profiles: any = {}; // Define the type of your profile data here
+  reservations: any[] = []; // Array to hold reservations
+  token = localStorage.getItem('token');
+  userID: string | undefined;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.loadUserProfileAndReservations();
+  }
+
+  loadUserProfileAndReservations(): void {
     const httpOptions = {
       headers: new HttpHeaders({
         Authorization: 'Bearer ' + this.token,
       }),
       withCredentials: true,
     };
-    
-    this.http.get<any>(`http://localhost:8088/profiles/get-id`,httpOptions)
-    .subscribe(
-      (res: any) => {
-        console.log("get id res: "+ res.user_id)
-        this.userID=res.user_id
-        this.profiles.push(res); // Add the retrieved profile to the profiles array
-        this.http.get<any>(`http://localhost:8088/profiles/${this.userID}`)
-        .subscribe(
-          (res1: any) => {
-            this.profiles.push(res1); // Add the retrieved profile to the profiles array
-          },
-          (error) => {
-            console.error(error);
+
+    this.http.get<any>(`http://localhost:8088/profiles/get-id`, httpOptions)
+      .subscribe(
+        (res: any) => {
+          this.userID = res.user_id;
+          if (this.userID) {
+            this.http.get<any>(`http://localhost:8088/profiles/${this.userID}`, httpOptions)
+              .subscribe(
+                (res1: any) => {
+                  this.profiles = res1; // Assign retrieved profile data
+                },
+                (error) => {
+                  console.error(error);
+                }
+              );
+
+            // Fetch reservations for this user
+            this.http.get<any>(`http://localhost:8002/reservations/by_guest/${this.userID}`, httpOptions)
+              .subscribe(
+                (reservations: any) => {
+                  this.reservations = reservations; // Assign retrieved reservations
+                },
+                (error) => {
+                  console.error(error);
+                }
+              );
           }
-        );
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-    // Fetch a specific user profile by ID
-   
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }
+
+  cancelReservation(reservationId: string, roomId: string): void {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + this.token,
+        'Content-Type': 'application/json', // Specify JSON content type
+      }),
+      withCredentials: true,
+      body: JSON.stringify({ reservation_id: reservationId, room_id: roomId }), // Prepare the JSON body
+    };
+  
+    this.http
+      .request<any>('delete', `http://localhost:8002/reservations/cancel`, httpOptions)
+      .subscribe(
+        (response: any) => {
+          console.log('Reservation canceled:', response);
+          // Update the UI or handle the canceled reservation response here
+          // For example, remove the canceled reservation from the 'reservations' array
+          this.reservations = this.reservations.filter((res) => res.reservation_id !== reservationId);
+        },
+        (error) => {
+          console.error('Cancellation failed:', error);
+          // Handle error response if needed
+        }
+      );
   }
 }
